eff9e483d5175f4d950dad64d278b80cd4b9c5c6
Daniel Sauvé <djsauve@rogers.com>
2017-12-30 11:23:56 -0500

    Decided to move Day 24 into one method to improve speed

--- Day 24/day24.dart
+++ Day 24/day24.dart
@@ -2,33 +2,39 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  new File('day24.txt').readAsString().then((String contents) {
-    print(strongestBridge(contents));
-    print(longestBridge(contents));
-  });
+  new File('day24.txt').readAsString().then(strongestBridge);
 }
 
-int strongestBridge(String puzzle) {
+void strongestBridge(String puzzle) {
   List<List<int>> l = puzzle
       .split('\n')
       .map((String line) => line.split('/').map(int.parse).toList())
       .toList();
   int strongest = 0;
+  Map<int, int> bridges = new Map();
   for (List<int> zero in l.where((List<int> pair) => pair.contains(0))) {
     strongest = max(strongest,
-        recurseStrength([zero], zero.firstWhere((int item) => item != 0), l));
+        recurse([zero], zero.firstWhere((int item) => item != 0), l, bridges));
   }
-  return strongest;
+  int longest = bridges[bridges.keys.reduce((int a, b) => max(a, b))];
+  print('Strongest: $strongest');
+  print('Longest: $longest');
 }
 
-int recurseStrength(
-    List<List<int>> bridge, int lookingFor, List<List<int>> pieces) {
+int recurse(
+    List<List<int>> bridge, int lookingFor, List<List<int>> pieces, Map<int, int> bridges) {
   int strongest = 0;
   List<List<int>> possibilities = pieces
       .where((List<int> item) =>
           item.contains(lookingFor) && !bridge.contains(item))
       .toList();
   if (possibilities.length == 0) {
+    bridges[bridge.length] = max(
+        bridges.putIfAbsent(bridge.length, () => 0),
+        bridge.fold(
+            0,
+                (int prev, List<int> item) =>
+            prev += item.reduce((int a, b) => a + b)));
     return bridge.fold(0,
         (int prev, List<int> item) => prev += item.reduce((int a, b) => a + b));
   }
@@ -40,46 +46,7 @@ int recurseStrength(
       lf = possibility.first;
     }
     strongest = max(strongest,
-        recurseStrength(new List.from(bridge)..add(possibility), lf, pieces));
+        recurse(new List.from(bridge)..add(possibility), lf, pieces, bridges));
   }
   return strongest;
-}
-
-int longestBridge(String puzzle) {
-  List<List<int>> l = puzzle
-      .split('\n')
-      .map((String line) => line.split('/').map(int.parse).toList())
-      .toList();
-  Map<int, int> possibilities = new Map();
-  for (List<int> zero in l.where((List<int> pair) => pair.contains(0))) {
-    recurseLength(
-        [zero], zero.firstWhere((int item) => item != 0), l, possibilities);
-  }
-  return possibilities[possibilities.keys.reduce((int a, b) => max(a, b))];
-}
-
-void recurseLength(List<List<int>> bridge, int lookingFor,
-    List<List<int>> pieces, Map<int, int> bridges) {
-  List<List<int>> possibilities = pieces
-      .where((List<int> item) =>
-          item.contains(lookingFor) && !bridge.contains(item))
-      .toList();
-  if (possibilities.length == 0) {
-    bridges[bridge.length] = max(
-        bridges.putIfAbsent(bridge.length, () => 0),
-        bridge.fold(
-            0,
-            (int prev, List<int> item) =>
-                prev += item.reduce((int a, b) => a + b)));
-    return;
-  }
-  for (List<int> possibility in possibilities) {
-    int lf;
-    try {
-      lf = possibility.firstWhere((int item) => item != lookingFor);
-    } catch (IterableElementError) {
-      lf = possibility.first;
-    }
-    recurseLength(new List.from(bridge)..add(possibility), lf, pieces, bridges);
-  }
 }
