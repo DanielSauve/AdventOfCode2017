5b83b9975df54ea55bde255e0e9d26caadc625b5
daniel.sauve <daniel.sauve@jsitelecom.com>
2018-01-05 11:06:14 -0500

    Rotate for 3x3 was blatantly wrong

--- Day21/day21.dart
+++ Day21/day21.dart
@@ -14,14 +14,14 @@ int doTheThing(String puzzle) {
         parts[0].split('/').map((String s) => s.split('')).toList();
     List<List<String>> to =
         parts[1].split('/').map((String s) => s.split('')).toList();
-    input[from.toString()] = to;
-    input[verticalFlip(from).toString()] = to;
-    input[horizontalFlip(from).toString()] = to;
+    input[listToString(from)] = to;
+    input[listToString(horizontalFlip(from))] = to;
+    input[listToString(verticalFlip(from))] = to;
     for (int i = 0; i < 3; i++) {
       from = rotate(from);
-      input[from.toString()] = to;
-      input[verticalFlip(from).toString()] = to;
-      input[horizontalFlip(from).toString()] = to;
+      input[listToString(from)] = to;
+      input[listToString(horizontalFlip(from))] = to;
+      input[listToString(verticalFlip(from))] = to;
     }
   }
   List<List<String>> start = [
@@ -29,9 +29,15 @@ int doTheThing(String puzzle) {
     ['.', '.', '#'],
     ['#', '#', '#']
   ];
+  for (String key in input.keys) {
+    if (key == listToString(start)) {
+      print(key);
+      input[key].forEach(print);
+    }
+  }
   for (int i = 0; i < 5; i++) {
     if (i == 0) {
-      start = input[start.toString()];
+      start = input[listToString(start)];
     } else if (start.length % 2 == 0) {} else {}
   }
   return start.fold(
@@ -50,42 +56,31 @@ List<List<String>> rotate(List<List<String>> toRotate) {
     return ret;
   } else {
     List<List<String>> ret = new List.generate(3, (_) => new List(3));
-    ret[0][0] = toRotate[1][0];
-    ret[0][1] = toRotate[0][0];
-    ret[0][2] = toRotate[0][1];
-    ret[1][0] = toRotate[2][0];
-    ret[1][2] = toRotate[0][2];
-    ret[2][0] = toRotate[2][1];
-    ret[2][1] = toRotate[2][2];
-    ret[2][2] = toRotate[1][2];
+    ret[0][0] = toRotate[2][0];
+    ret[0][1] = toRotate[1][0];
+    ret[0][2] = toRotate[0][0];
+    ret[1][0] = toRotate[2][1];
     ret[1][1] = toRotate[1][1];
+    ret[1][2] = toRotate[0][1];
+    ret[2][0] = toRotate[2][2];
+    ret[2][1] = toRotate[1][2];
+    ret[2][2] = toRotate[0][2];
     return ret;
   }
 }
 
 List<List<String>> verticalFlip(List<List<String>> toFlip) {
-  if (toFlip.length == 2) {
-    List<List<String>> ret = new List.generate(2, (_) => new List(2));
-    ret[0][0] = toFlip[1][0];
-    ret[0][1] = toFlip[1][1];
-    ret[1][0] = toFlip[0][0];
-    ret[1][1] = toFlip[0][1];
-    return ret;
-  } else {
-    List<List<String>> ret = new List.generate(3, (_) => new List(3));
-    ret[0][0] = toFlip[2][0];
-    ret[0][1] = toFlip[2][1];
-    ret[0][2] = toFlip[2][2];
-    ret[1][0] = toFlip[1][0];
-    ret[1][1] = toFlip[1][1];
-    ret[1][2] = toFlip[1][2];
-    ret[2][0] = toFlip[0][0];
-    ret[2][1] = toFlip[0][1];
-    ret[2][2] = toFlip[0][2];
-    return ret;
-  }
+  return toFlip.reversed.toList();
 }
 
 List<List<String>> horizontalFlip(List<List<String>> toFlip) {
   return toFlip.map((List<String> row) => row.reversed.toList()).toList();
+}
+
+String listToString(List<List<String>> list) {
+  if (list.length == 2) {
+    return list[0].join('') + '/' + list[1].join('');
+  } else {
+    return list[0].join('') + '/' + list[1].join('') + '/' + list[2].join('');
+  }
 }
